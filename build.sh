71b8fa946eb5d9f230d9c7b570d8751c5567211d
r0fus0d <d2hwakh7bs5e@protonmail.com>
2023-12-19 18:49:14 +0800

    update 2023年12月19日 星期二 18时49分13秒 CST

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 rm -rf trufflehog-amd64 trufflehog-arm64
 
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o trufflehog main.go
-#upx -9 trufflehog
-#mv trufflehog trufflehog-amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o trufflehog main.go
+upx -9 trufflehog
+mv trufflehog trufflehog-amd64
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o trufflehog main.go
 upx -9 trufflehog
