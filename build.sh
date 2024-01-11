729038d17f55fc972225b830cfb5f16a7e4672ed
r0fus0d <d2hwakh7bs5e@protonmail.com>
2024-01-11 16:00:13 +0800

    update 2024年 1月11日 星期四 16时00分13秒 CST

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 # goreleaser --snapshot --skip-publish --rm-dist
 
-rm -rf trufflehog-amd64 trufflehog-arm64
+rm -rf trufflehog-amd64-linux trufflehog-arm64-linux trufflehog-arm64-darwin
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o trufflehog main.go
+mv trufflehog trufflehog-arm64-darwin
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o trufflehog main.go
 upx -9 trufflehog
-mv trufflehog trufflehog-amd64
+mv trufflehog trufflehog-amd64-linux
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o trufflehog main.go
 upx -9 trufflehog
-mv trufflehog trufflehog-arm64
+mv trufflehog trufflehog-arm64-linux
